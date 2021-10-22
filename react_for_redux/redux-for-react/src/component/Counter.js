54805916b9a3a5538d668a63346c7a7b310f25c1
sujanchhengutala <47767420+sujanchhengutala@users.noreply.github.com>
2021-10-22 21:00:30 +0545

    uninstall redux and install redux toolkit

--- react_for_redux/redux-for-react/src/component/Counter.js
+++ react_for_redux/redux-for-react/src/component/Counter.js
@@ -1,12 +1,42 @@
-import { useSelector } from "react-redux"
-
+import { useSelector, useDispatch } from "react-redux"
+import classes from './Counter.module.css'
 
 const Counter = ()=>{
+    const dispatch = useDispatch()
     const counter = useSelector( state => state.counter)
-    return(
-    <>
-    <div> {counter} </div>
-    </>
+    const show = useSelector(state => state.showCounter)
+
+    const incrementHandaler = ()=>{
+        dispatch({type: 'increment'})
+
+    }
+    const increaseHandaler = ()=>{
+        dispatch({type: 'increase', amount: 10})
+
+    }
+    
+    const decrementHandaler = ()=>{
+        dispatch({type: 'decrement'})
+    }
+
+    const toggleCounterHandler = ()=>{
+        dispatch({type: 'toogle'})
+        
+    }
+
+
+
+
+    return(<main className = {classes.counter}>
+     <h1>Redux Counter</h1>
+     {show && <div className = {classes.value}> {counter} </div>}
+    <div>
+    <button onClick = { incrementHandaler }>increment</button>
+    <button onClick = { increaseHandaler }>increase</button>
+    <button onClick = { decrementHandaler }>decrement</button>
+    <button onClick = {toggleCounterHandler}>toggle Counter</button>
+    </div>
+    </main>
     )
 }
 export default Counter;
--- react_for_redux/redux-for-react/src/store/index.js
+++ react_for_redux/redux-for-react/src/store/index.js
@@ -1,10 +1,17 @@
 import { createStore } from 'redux'
-const counterReducer = (state = { counter : 10 }, action) =>{
-    if( action.type === 'increase') {
-        return {counter : state.counter + 1 }
+const initialstate = { counter : 10, showCounter : true}
+const counterReducer = (state = initialstate, action) =>{
+    if( action.type === 'increment') {
+        return {counter : state.counter + 1, showCounter : state.showCounter }
     }
-    if( action.type === 'decrease') {
-        return {counter : state.counter - 1 }
+    if( action.type === 'increase') {
+        return {counter : state.counter + action.amount, showCounter : state.showCounter }
+    }
+    if( action.type === 'decrement') {
+        return {counter : state.counter - 1, showCounter : state.showCounter }
+    }
+    if( action.type === 'toogle') {
+        return {counter : state.counter, showCounter : !state.showCounter }
     }
     return state;
 }
